cb53febc1ee1afc6ac1ac7be44cbb58f4bf7d7d2
TeohJianPeng <71300278+TeohJianPeng@users.noreply.github.com>
2020-12-29 09:08:42 +0800

    Add files via upload

--- LaplaceEigenvalue.m
+++ LaplaceEigenvalue.m
@@ -1,28 +1,30 @@
 N = 100;                 %矩陣的大小
-Delta = 1/(N-1);         %分出100個點/99段
+Delta = 1/(N+1);         %分出100個點/99段
 
-u = rand(N*N,1);         %隨機的2維矩陣
+u = [0;ones(N*N-2,1);2];
+%u = rand(N*N,1);         %隨機的2維矩陣
 
 ite1 = 0;                %進行的次數
-tol = 10^(-4);           %誤差值
+tol = 10^(-9);           %誤差值
 a = 0;                   %lambda的初始值
 L = sparse(N*N,N*N);      %開一個10000*10000的全零矩陣
 
+%把function變成矩陣
 for ii = 1:N*N
-    L(ii,ii)=-4;
-    if rem(ii-1,N)>0
-        L(ii,ii-1)=1;
-    end
-    if rem(ii,N) > 0
-        L(ii,ii+1)=1;
-    end
-    if ii-N > 0
-        L(ii,ii-N)=1;
-    end
-    if ii+N <= N*N
-        L(ii,ii+N)=1;
-    end
-end
+     L(ii,ii)=-4;        %中間對角線的位置變成4
+     if rem(ii-1,N)>0
+         L(ii,ii-1)=1;
+     end
+     if rem(ii,N) > 0
+         L(ii,ii+1)=1;
+     end
+     if ii-N > 0
+         L(ii,ii-N)=1;
+     end
+     if ii+N <= N*N
+         L(ii,ii+N)=1;
+     end
+ end
 
 L = L/(Delta*Delta);
 
@@ -31,16 +33,13 @@ while 1
     u = u/norm(u);         %u的長度是1
     v = L*u;
     lambda = v(1)/u(1);
-    if abs(lambda-a)/abs(a) < tol
+    if abs(lambda-a)/abs(a) < tol  %判斷收斂值
         break
     end
     a = lambda;
     u = v;
 end
 maxlambda = abs(lambda)
-% 
-% K = inv(L);
-% K = L/v;
 
 a = 0;
 ite2 = 0;
@@ -49,7 +48,7 @@ while 1
     u = u/norm(u);         %讓u的長度是1
     v = L\u;
     lambda2 = v(1)/u(1);
-    if abs(lambda2-a)/abs(a) < tol
+    if abs(lambda2-a)/abs(a) < tol  %判斷收斂值
         break
     end
     a = lambda2;
